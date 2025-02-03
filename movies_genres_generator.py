1a1d0f29fee3407716ee4247c6c0e542fc20a5f6
Vicen <150998620+Vilicaprogramer@users.noreply.github.com>
2025-02-03 13:26:57 +0100

    Añadiendo el modelo con Git LFS

--- movies_genres_generator.py
+++ movies_genres_generator.py
@@ -1,6 +1,7 @@
 # Importamos todas las librerías necesarias
 import joblib
 import streamlit as st
+import preprocesing_txt as pp
 
 # Cargamos el modelo de clasificación de géneros de películas
 model = joblib.loac('cc_model.pkl')

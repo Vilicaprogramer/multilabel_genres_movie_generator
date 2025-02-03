8c753f346efa18a7ed40ecc2930d6d2a3b000321
Vicen <150998620+Vilicaprogramer@users.noreply.github.com>
2025-02-03 13:07:45 +0100

    Creación de archivos para el generador y carga del modelo

---
+++ movies_genres_generator.py
@@ -0,0 +1,8 @@
+# Importamos todas las librerías necesarias
+import joblib
+import streamlit as st
+
+# Cargamos el modelo de clasificación de géneros de películas
+model = joblib.loac('cc_model.pkl')
+
+
